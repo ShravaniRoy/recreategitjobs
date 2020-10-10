ac64eb87347f59af41ad06921b29fc6c9039fbc9
Shravani Roy <shravani.thirunagari@gmail.com>
2020-10-10 14:35:39 +0530

    store using hooks is setup without redux

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,6 +1,32 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
+
+const ACTIONS = {
+    MAKE_REQUEST : 'make_request',
+    GET_DATA : 'get_data',
+    ERROR : 'error'
+}
+function reducer(state, action){
+    switch(Selection.type){
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, jobs: [], error: action.payload.error }
+        default:
+            return state
+
+    }
+}
 
 export default function useFetchJobs(params, page){
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false, error: false, jobs: [4, 5, 6]
+    })
+
+    useEffect(()=> {
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+    }, params, page)
     return {
         jobs: [4, 5, 6],
         loading: false,
