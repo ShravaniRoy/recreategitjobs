031a1b878cf74ccb8325eb2b9a28c17d653ac208
Shravani Roy <shravani.thirunagari@gmail.com>
2020-10-11 01:21:11 +0530

    jobs and pagination added, error in jobs rendering

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,5 +1,8 @@
 import { useReducer, useEffect } from 'react'
+import axios from 'axios'
+//https://cors-anywhere.herokuapp.com/ this enables cors policy by performing as third party proxy for us.
 
+const GET_DATA_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?'
 const ACTIONS = {
     MAKE_REQUEST : 'make_request',
     GET_DATA : 'get_data',
@@ -21,15 +24,21 @@ function reducer(state, action){
 
 export default function useFetchJobs(params, page){
     const [state, dispatch] = useReducer(reducer, {
-        loading: false, error: false, jobs: [4, 5, 6]
+        loading: false, error: false, jobs: []
     })
 
     useEffect(()=> {
         dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(GET_DATA_URL, {
+            params : { markdown: true, page: page, ...params }
+        }).then(res => {
+            dispatch({
+                type: ACTIONS.GET_DATA, payload: { jobs: res.data }
+            })
+        }).catch(err => {
+            dispatch({ type: ACTIONS.ERROR, payload: {error: err} })
+        })
     }, params, page)
-    return {
-        jobs: [4, 5, 6],
-        loading: false,
-        error: false
-    }
+    return state
+    
 }
